fb8686ea866fceb5668a83e094109417c4367734
litswd <hiromu.kariya@gmail.com>
2023-06-19 22:46:34 +0900

    Modify default page

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let message = "Software Design!";
+  const prefix = "Svelte";
+</script>
+
+<h1>Hello, {message} from {prefix}</h1>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
