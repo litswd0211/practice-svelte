f942954aaecf2fa9077acc791ede77e6788f41d0
litswd <hiromu.kariya@gmail.com>
2023-06-19 22:52:08 +0900

    Add Child component

---
+++ myapp/src/lib/components/Child.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let text;
+  export let html;
+  export let arr;
+  export let obj;
+</script>
+
+<p class="text">{text}</p>
+
+<p class="html">{@html html}</p>
+
+<ul class="array-list">
+  {#each arr as ar, i}
+    <li>
+      <a href={ar.link}>
+        {i + 1} <span>{ar.text}</span>
+      </a>
+    </li>
+  {/each}
+</ul>
+
+<div class="object-card">
+  {#if obj.imgpath}
+    <img src={obj.imgpath} alt={obj.text} />
+  {/if}
+  <p class="text">{obj.text}</p>
+</div>
--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script>
-  let message = "Software Design!";
-  const prefix = "Svelte";
+  import Child from "$lib/components/Child.svelte";
+
+  let text = "text,txt";
+  let html = "1<br>2<br>3";
+  let arr = [
+    { link: "#1", text: "text01" },
+    { link: "#2", text: "text02" },
+    { link: "#3", text: "text03" },
+  ];
+  let obj = {
+    imgpath: "https://picsum.photos/200/300",
+    text: "text",
+  };
 </script>
 
-<h1>Hello, {message} from {prefix}</h1>
-
-<style>
-  h1 {
-    color: red;
-  }
-</style>
+<Child {text} {html} {arr} {obj} />
