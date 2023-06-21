5d2130a152bf5d2623c27c1381538bf908f8db60
litswd <hiromu.kariya@gmail.com>
2023-06-21 22:08:59 +0900

    Add lifecycle

--- myapp/src/lib/components/DispatcherChild.svelte
+++ myapp/src/lib/components/DispatcherChild.svelte
@@ -2,9 +2,27 @@
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
 
-  function handleCheck() {
-    dispatch("myevent", { data: "Custom event triggered" });
+  import { onMount, beforeUpdate, afterUpdate, tick } from "svelte";
+  let count = 0;
+
+  async function handleCheck() {
+    // dispatch("myevent", { data: "Custom event triggered" });
+    count++;
+    await tick();
+    console.log(`count: ${count}`);
   }
+
+  onMount(() => {
+    console.log("Component mounted");
+  });
+
+  beforeUpdate(() => {
+    console.log("Component will update");
+  });
+
+  afterUpdate(() => {
+    console.log("Component updated");
+  });
 </script>
 
 <button on:click={handleCheck}>Trigger Custom Event</button>
